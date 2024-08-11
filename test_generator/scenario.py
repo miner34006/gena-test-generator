973b00dba44abf1238b6e3cf1ebabc2ccf6e2896
Bogdan Polianok <miner34006@gmail.com>
2024-08-11 12:18:47 +0200

    refactor

--- test_generator/scenario.py
+++ test_generator/scenario.py
@@ -1,11 +1,5 @@
 from dataclasses import dataclass, field
 
-PARAMS_TEMPLATE = """
-    $params
-    def __init__(self, param):
-        self.param = param
-"""
-
 
 @dataclass
 class TestScenario:
@@ -18,19 +12,3 @@ class TestScenario:
     params: list[str] = field(default_factory=list, repr=False)
 
     other_data: dict = field(default_factory=dict, repr=False)
-
-    def update_subject(self) -> None:
-        append_str = " (param = {{param}})"
-        if self.params and append_str not in self.subject:
-            self.subject += " (param = {{param}})"
-
-    def get_params_template(self, tab_size: int = 4) -> str:
-        if not self.params:
-            return ''
-
-        params_str = ''
-        for i, param in enumerate(self.params):
-            params_str += f'@vedro.params("{param}")'
-            if i != len(self.params) - 1:
-                params_str += '\n' + ' ' * tab_size
-        return PARAMS_TEMPLATE.replace('$params', params_str)
--- test_generator/test_handlers/vedro_handler.py
+++ test_generator/test_handlers/vedro_handler.py
@@ -5,6 +5,11 @@ from test_generator.scenario import TestScenario
 from test_generator.suite import Suite
 from test_generator.test_handlers.test_handler import TestHandler
 
+PARAMS_TEMPLATE = """\n
+    $params
+    def __init__(self, param):
+        self.param = param"""
+
 
 class VedroHandler(TestHandler):
     def __init__(self, template: str) -> None:
@@ -15,14 +20,13 @@ class VedroHandler(TestHandler):
         raise NotImplementedError('method is not implemented')
 
     def write_test(self, file_path: str, scenario: TestScenario, feature: str, story: str, *args, **kwargs) -> None:
-        scenario.update_subject()
         filled_template = self.template.replace('$feature', feature) \
                                        .replace('$story', story) \
                                        .replace('$priority', scenario.priority) \
-                                       .replace('$subject', scenario.subject) \
+                                       .replace('$subject', self.__get_subject(scenario)) \
                                        .replace('$description', scenario.description) \
                                        .replace('$expected_result', scenario.expected_result) \
-                                       .replace('$params', scenario.get_params_template())
+                                       .replace('$params', self.__get_params(scenario))
 
         with open(file_path, 'w', encoding='utf-8') as file:
             file.write(filled_template)
@@ -51,3 +55,22 @@ class VedroHandler(TestHandler):
                 raise ScenariosValidationError(f'Subject is not defined for scnenario {scenario}')
             if not scenario.test_name:
                 raise ScenariosValidationError(f'Test name is not defined for scnenario {scenario}')
+
+    def __get_subject(self, scneario: TestScenario) -> str:
+        append_str = " (param = {{param}})"
+
+        subject = scneario.subject
+        if scneario.params and append_str not in scneario.subject:
+            subject = f"{scneario.subject} (param = {{param}})"
+        return subject
+
+    def __get_params(self, scneario: TestScenario, tab_size: int = 4) -> str:
+        if not scneario.params:
+            return ''
+
+        params_str = ''
+        for i, param in enumerate(scneario.params):
+            params_str += f'@vedro.params("{param}")'
+            if i != len(scneario.params) - 1:
+                params_str += '\n' + ' ' * tab_size
+        return PARAMS_TEMPLATE.replace('$params', params_str)
